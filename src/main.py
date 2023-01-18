f2978452d6f8bfb0e3ecd78857540fc0d0e3c71c
Vincent C. Mader <vincent.mader@icloud.com>
2023-01-18 17:40:30 +0100

    added plot for error as a function of $\Delta x$

--- src/main.py
+++ src/main.py
@@ -5,8 +5,11 @@ import numpy as np
 
 from finite_differences import METHODS
 
-XMIN, XMAX, J = -0, 10, 100
-DX = (XMAX-XMIN)/J
+XMIN, XMAX, NX = -0, 10, 100
+DX = (XMAX-XMIN) / NX
+
+HMIN, HMAX, NH = 1e-15, 1e0, 100
+X0 = 0
 
 MPL_THEME = "./mpl-styles/dark.mplstyle"
 plt.style.use(MPL_THEME)
@@ -38,14 +41,31 @@ def plot_2(x, f):
     plt.ylabel("error $f_{num}(x) - f(x)$")
     plt.legend(loc="upper left")
     plt.title("comparison of 1st order finite difference scheme error")
-    path = os.path.join("..", "out", "fd-errors.png")
+    path = os.path.join("..", "out", "fd-error-vs-x.png")
+    plt.savefig(path)
+    plt.show()
+
+
+def plot_3(f):
+    h_range = np.logspace(-15, 0, 100)
+    for (label, method) in METHODS:
+        Y = method([X0], f, h_range)
+        err = Y / f(X0)
+        plt.plot(x, err, label=label)
+
+    plt.xlabel("$\Delta x$")
+    plt.ylabel("error $f_{num}(x) / f(x)$")
+    plt.legend(loc="upper left")
+    plt.title("comparison of 1st order finite difference scheme error")
+    path = os.path.join("..", "out", "fd-error-vs-h.png")
     plt.savefig(path)
     plt.show()
 
 
 if __name__ == "__main__":
     f = np.exp
-    x = np.linspace(XMIN, XMAX, J)
+    x = np.linspace(XMIN, XMAX, NX)
 
     plot_1(x, f)
     plot_2(x, f)
+    plot_3(f)
